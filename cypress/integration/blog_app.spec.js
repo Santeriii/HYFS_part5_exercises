9552508c5fa37e9b14a0a0a71f9b098cbbfa7a9c
Santeri Einola <62256499+Santeriii@users.noreply.github.com>
2020-11-24 12:44:23 +0200

    Exercise 5.17 done

---
+++ cypress/integration/blog_app.spec.js
@@ -0,0 +1,21 @@
+describe('Blog app', function() {
+    beforeEach(function() {
+      cy.request('POST', 'http://localhost:3001/api/testing/reset')
+      const user = {
+          name: 'Testi',
+          username: 'testi',
+          password: 'testi'
+      }
+      cy.request('POST', 'http://localhost:3001/api/users/', user)
+      cy.visit('http://localhost:3000')
+    })
+  
+    it('Login form is shown', function() {
+      cy.contains('Login').click()
+      cy.get('#username').type('testi')
+      cy.get('#password').type('testi')
+      cy.get('#login-button').click()
+
+      cy.contains('Testi logged in')
+    })
+  })
